c41054b6da2695d84635b7b334d4feb827f33f0b
Chong <916681033@qq.com>
2020-06-28 18:17:25 +0800

    Allow InstanceSigs

--- Num/Nat.hs
+++ Num/Nat.hs
@@ -1,13 +1,15 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Num.Nat where
 
 -- Nat: natural numbers, Z is 0, S is succ
 data Nat = Z | S Nat deriving (Show, Eq, Ord)
 
 instance Enum Nat where
-    -- toEnum :: Int -> Nat
+    toEnum :: Int -> Nat
     toEnum 0 = Z
     toEnum i = S $ toEnum $ pred i
-    -- fromEnum :: Nat -> Int
+    fromEnum :: Nat -> Int
     fromEnum Z = 0
     fromEnum (S n) = succ (fromEnum n)
     
